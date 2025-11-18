80a7bbd4c0c4e74d8b3a2e669363e5b2a3674b70
sahir zakaria <sahir.alzakaria@gmail.com>
2025-11-18 13:32:53 +0300

    add containers to rightBar

--- src/components/rightBar/RightBar.jsx
+++ src/components/rightBar/RightBar.jsx
@@ -1,8 +1,107 @@
 import './rightBar.scss'
-
+import personImg from '../../assets/person.jpg';
 const RightBar = () => {
   return (
-    <div className='rightBar'>RightBar</div>
+    <div className='rightBar'>
+      <div className="container">
+        <div className="item">
+          <span>Suggestions For You</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src={personImg} alt="" />
+              <span>Ahmed Ali</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src={personImg} alt="" />
+              <span>Ahmed Ali</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src={personImg} alt="" />
+              <span>Ahmed Ali</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src={personImg} alt="" />
+              <span>Ahmed Ali</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+        </div>
+        <div className="item">
+          <span>Latest Activities</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src={personImg} alt="" />
+              <p>
+                <span>Ahmed Ali</span> changed their cover picture
+              </p>
+
+            </div>
+            <span>1 minute ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src={personImg} alt="" />
+              <p>
+                <span>Ahmed Ali</span> changed their cover picture
+              </p>
+
+            </div>
+            <span>1 minute ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src={personImg} alt="" />
+              <p>
+                <span>Ahmed Ali</span> changed their cover picture
+              </p>
+
+            </div>
+            <span>1 minute ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src={personImg} alt="" />
+              <p>
+                <span>Ahmed Ali</span> changed their cover picture
+              </p>
+
+            </div>
+            <span>1 minute ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src={personImg} alt="" />
+              <p>
+                <span>Ahmed Ali</span> changed their cover picture
+              </p>
+
+            </div>
+            <span>1 minute ago</span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
