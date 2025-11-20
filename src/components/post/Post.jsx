646ed8dc7c3b878c55c7a1cd9145e674a104a6e4
sahir zakaria <sahir.alzakaria@gmail.com>
2025-11-20 14:33:56 +0300

    add themed to posts

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -9,23 +9,24 @@ import { Link } from 'react-router-dom';
 const Post = ({ post }) => {
     return (
         <div className='post'>
-            <div className="user">
-                <div className="userInfo">
-                    <img src={post.img} alt="" />
-
-                    <div className="details">
-                        <Link to={`/profile/${post.userId}`} style={{ textDecoration: "none", color: "inherit" }}>
-                            <span>{post.name}</span>
-                        </Link>
+            <div className="container">
+                <div className="user">
+                    <div className="userInfo">
+                        <img src={post.profilePic} alt="" />
+                        <div className="details">
+                            <Link to={`/profile/${post.userId}`} style={{ textDecoration: "none", color: "inherit" }}>
+                                <span className='name'>{post.name}</span>
+                            </Link>
+                            <span className="date">1 min ago</span>
+                        </div>
                     </div>
-                    <div className="username"></div>
+                    <MoreHorizIcon />
                 </div>
-                <MoreHorizIcon />
+                <div className="content"></div>
+                <div className="info"></div>
             </div>
-            <div className="content"></div>
-            <div className="info"></div>
         </div>
-    )
-}
+    );
+};
 
 export default Post
