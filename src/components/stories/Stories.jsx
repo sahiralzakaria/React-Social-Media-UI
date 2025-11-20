1c770066d3b79025532f47e7b6a0c92e3034c838
sahir zakaria <sahir.alzakaria@gmail.com>
2025-11-20 12:34:41 +0300

    add post component

--- src/components/stories/Stories.jsx
+++ src/components/stories/Stories.jsx
@@ -1,11 +1,16 @@
 import './stories.scss'
-import person2 from '../../assets/person2.jpg'
-// import person3 from '../../assets/person3.jpg'
-import person4 from '../../assets/person4.jpg'
-import person5 from '../../assets/person5.jpg'
-import person6 from '../../assets/person6.jpg'
-// import person7 from '../../assets/person7.jpg'
+import person2 from '../../assets/person2.png'
+// import person3 from '../../assets/person3.png'
+import person4 from '../../assets/person4.png'
+import person5 from '../../assets/person5.png'
+import person6 from '../../assets/person6.png'
+import { useContext } from 'react'
+import { AuthContext } from '../../context/authContext'
+// import person7 from '../../assets/person7.png'
+
 const Stories = () => {
+
+    const { currentUser } = useContext(AuthContext);
 
     //TEMPORARY
     const stories = [
@@ -45,9 +50,14 @@ const Stories = () => {
     ]
     return (
         <div className='stories'>
+            <div className="story">
+                <img src={currentUser.profilePicture} alt="" />
+                <span>{currentUser.name}</span>
+                <button>+</button>
+            </div>
             {stories.map(story => (
 
-                <div className="story">
+                <div className="story" key={story.id}>
                     <img src={story.img} alt="" />
                     <span>{story.name}</span>
                 </div>
